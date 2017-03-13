ab749e1c9ccb8cafec8e48e57ac2a6245eefb7d5
Kevin Luman <kevinleeluman@gmail.com>
2017-03-12 21:34:24 -0400

    Updates to “me” controller and template.

--- src/main/java/com/example/about/web/AboutController.java
+++ src/main/java/com/example/about/web/AboutController.java
@@ -1,5 +1,30 @@
 package com.example.about.web;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+import java.util.Map;
+
+import com.example.about.domain.Person;
+import com.example.about.domain.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/me")
+public class AboutController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public void setPersonRepository(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping
+    public String me(Map<String, Object> model) {
+        Person me = personRepository.findOne(1L);
+        model.put("me", me);
+
+        return "me";
+    }
 }
