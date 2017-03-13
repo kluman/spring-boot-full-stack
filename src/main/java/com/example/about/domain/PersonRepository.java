c7b1997182d6f1260bdc1dfb4f662ab609c04cac
Kevin Luman <kevinleeluman@gmail.com>
2017-03-12 21:34:03 -0400

    Adds controller for mapping Person model to “/me” requests and mustache template.

--- src/main/java/com/example/about/domain/PersonRepository.java
+++ src/main/java/com/example/about/domain/PersonRepository.java
@@ -1,6 +1,5 @@
 package com.example.about.domain;
 
-import com.example.about.domain.Person;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
---
+++ src/main/java/com/example/about/web/AboutController.java
@@ -0,0 +1,5 @@
+package com.example.about.web;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+}
